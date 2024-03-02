99b6f462fbc3a73b3741020199d9ba830c7da4b9
citlallixx03 <2282785651citlali@gmail.com>
2024-03-02 14:23:55 -0600

    miniproyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import TextTemplate from './TextTemplate';
+
+function App() {
+  return (
+    <div>
+      <TextTemplate />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TextTemplate.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TextTemplate() {
+  return (
+    <div>
+      <h1>Bienvenido a mi pequeña página web</h1>
+      <p>Este es un ejemplo de una página web simple utilizando React y Vite.</p>
+    </div>
+  );
+}
+
+export default TextTemplate;
